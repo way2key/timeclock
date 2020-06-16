b9d7669429f7e87684d30f11a328544fbf39d362
way2key <olivier_dancona@hotmail.com>
2020-06-16 15:06:54 +0200

    Fixed Console Bug

--- clockmachine/backend/planner.js
+++ clockmachine/backend/planner.js
@@ -18,7 +18,7 @@ const action = require('./action/action');
 
  https://www.npmjs.com/package/node-cron
 */
-//action.createDayForEachUser();
+
 // Crée les days tous les jours à 00:00:01s
 cron.schedule('00 00 00 * * *', () => {
   action.createDayForEachUser();
